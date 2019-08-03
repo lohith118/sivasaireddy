71e2e1b70ce08a240834129b73844e4d02489bff
lohith118 <52988905+lohith118@users.noreply.github.com>
2019-08-03 12:37:23 +0530

    Update gill.py

--- gill.py
+++ gill.py
@@ -1,8 +1,8 @@
 chaa=int(input())
-choa=[int(o) for o in input().split(" ")]
+ch=[int(o) for o in input().split(" ")]
 chea=0
 for p in range(chaa):
       for g in range(p):
-           if(choa[g]<choa[p]):
-                chea+=choa[g]
+           if(ch[g]<choa[p]):
+                chea+=ch[g]
 print(chea)
