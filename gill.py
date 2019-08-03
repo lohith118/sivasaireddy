7d6f230d071309eaf1f0882ec58fa246708ea913
lohith118 <52988905+lohith118@users.noreply.github.com>
2019-08-03 12:40:24 +0530

    Update and rename gill.py to gil.py

--- gill.py
+++
@@ -1,8 +0,0 @@
-chaa=int(input())
-ch=[int(o) for o in input().split(" ")]
-chea=0
-for p in range(chaa):
-      for g in range(p):
-           if(ch[g]<choa[p]):
-                chea+=ch[g]
-print(chea)
